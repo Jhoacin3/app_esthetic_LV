075f6088a5dd0c4eeaef10b8a0535e89ee6a3018
your <mrubi366@gmail.com>
2023-11-23 18:29:07 -0600

    edite el login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -30,65 +30,73 @@ const submit = () => {
 </script>
 
 <template>
-    <GuestLayout>
-        <Head title="Log in" />
+    <GuestLayout >
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
+<Head title="Log in" />
+
+
+<div v-if="status" class="mb-4 font-medium text-sm text-green-600">
+    {{ status }}
+</div>
+
+<form @submit.prevent="submit">
+
+
+
+    <div>
+        <InputLabel for="email" value="Correo electrónico" />
+
+        <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus
+            autocomplete="username" />
+
+        <InputError class="mt-2" :message="form.errors.email" />
+
+    </div>
+
+
+
+    <div class="mt-8">
+        <InputLabel for="password" value="Contraseña" />
+
+        <TextInput id="password"  type="password" class="mt-1 block w-full" v-model="form.password" required=""
+            autocomplete="current-password" />
+
+           <InputError class="mt-2" :message="form.errors.password" />
+    </div>
+  
+    
+
+    <div class="block mt-8 flex items-center justify-between">
+        <label class="flex items-center">
+            <Checkbox name="remember" v-model:checked="form.remember" />
+            <span class="ml-2 text-sm text-gray-600">Recordar Contraseña</span>
+        </label>
+
+        <div class="text-sm text-right">
+            <Link v-if="canResetPassword" :href="route('password.request')"
+                class="font-semibold text-cyan-600 hover:text-cyan-700">
+            Olvidastes tu contraseña?
+            </Link>
         </div>
+    </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
+    
 
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
+    <div class="flex items-center justify-end mt-8">
 
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+        <PrimaryButton  className="flex w-full justify-center rounded-md bg-cyan-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-rose-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-Fuchsia-600"
+      >
+        Iniciar Sesion
 
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-600">Recuerdame</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Olvidaste tu contraseña?
-                </Link>
-
-                <PrimaryButton class="ms-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Iniciar sesión
-                </PrimaryButton>
-            </div>
-        </form>
-    </GuestLayout>
+        </PrimaryButton>
+    </div>
+</form>
+<p className="mt-10 text-center text-sm text-gray-500">
+    ¿Aún no tienes una cuenta? 
+    <a href="register" className="font-semibold leading-6 text-cyan-600 hover:text-cyan-700">
+       Únete
+    </a>
+  </p>
+</GuestLayout>
 </template>
